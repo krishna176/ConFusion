d9ceaab25cd09069cbfe97647690b29db8bcd7a7
Krishna Kumar <thekrishna199@gmail.com>
2022-01-02 12:10:16 +0530

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
 function App() {
@@ -18,6 +19,14 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Navbar dark color="primary" >
+        <div className='container'>
+          <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+
+
     </div>
   );
 }
